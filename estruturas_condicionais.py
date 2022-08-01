ff9626cb829b80c65723045612fc26ede95dbbc5
Loiroo <henriquemc40@gmail.com>
2022-08-01 13:55:40 -0300

    first commit

---
+++ estruturas_condicionais.py
@@ -0,0 +1,12 @@
+" Estruturas condicionais "
+
+idade = 20
+
+if idade < 18:
+    print("Menor de Idade")
+elif idade == 18:
+    print("Tem 18 anos")
+elif idade == 20:
+    print("Tem 20 anos")
+else:
+    print("Maior de idade")
