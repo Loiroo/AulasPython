4b116d18888796ca71e06c7a6ad4e4e999da4954
Loiroo <henriquemc40@gmail.com>
2022-08-05 15:56:19 -0300

    commit exercícios 4 do cap4

---
+++ ExercíciosPython/ExercícioPython-cap4-4.py
@@ -0,0 +1,8 @@
+" Leia um número real e imprima o resultado do quadrado desse número "
+
+print("Digite um número:")
+numero = int(input())
+
+quadrado = (numero * numero)
+
+print("O quadrado de {0} é: {1}".format(numero,quadrado))
